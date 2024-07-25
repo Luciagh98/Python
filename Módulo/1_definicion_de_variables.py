43d75548b0342b287242702a766529ebe7845b29
Luciagh98 <luciagh98@gmail.com>
2024-07-25 10:49:32 +0200

    Create Módulo

---
+++ Módulo/1_definicion_de_variables.py
@@ -0,0 +1,30 @@
+#Comentario de una línea --> Definimos la variable x
+x = "Hola" #No necesita tipado
+print(x) #Mostrarla en pantalla
+
+#Modificamos el tipado
+x = 1
+print(x)
+
+#Varias definiciones en una lineas
+x,y = 1,2
+print(x)
+print(y)
+
+'''
+Comentarios de mas 
+de una linea
+'''
+
+"""
+Comillas dobles y simples
+"""
+
+#Indetación
+x = "Hola"
+def myfunc():
+  x = 5
+  print(x)
+
+myfunc()
+print(x)
---
+++ Módulo/2_tipos_basicos.py
@@ -0,0 +1,28 @@
+a = "Hola Mundo" #Cadena de caracteres
+print(a,type(a))
+
+b = 4 #Entero
+print(b,type(b))
+
+c = 5.2 #Decimal
+print(c,type(c))
+
+#Irreal
+d = 11e10 
+d = 11E19
+d = -13.8
+
+x = 3 + 5j
+
+x = "ASD\nAA"
+print(x)
+
+x = """ASD
+AA"""
+print(x)
+
+x = "ASD\tAA"
+print(x)
+
+x = "ASD\\AA"
+print(x)
